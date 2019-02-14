58ce565c59484df964e6dc903b1b330b48af7325
Hiromu17 <kuromu890@gmail.com>
2019-02-14 12:02:03 +0900

    headerのテンプレートを作成しました

---
+++ header.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+
+$login_e_msg = '';
+$nav_item = [];
+if(isset($_SESSION['user'])){
+    $nav_item['text'] = 'ログアウト';
+    $nav_item['src'] = 'logout.php';
+} else{
+    $nav_item['text'] = 'ログイン';
+    $nav_item['src'] = 'login.php';
+}
+
+function h($str) {
+    return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <header>
+                <div class="col-sm-10 offset-1">
+                    <nav class="navbar navbar-expand-sm justify-content-around">
+                        <h1>問い合わせ管理</h1>
+                        <ul class="nav">
+                            <li class="nav-item">
+                                <a class="btn btn-outline-info" href="form.php">案件追加</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="btn btn-outline-info" href="<?= $nav_item['src'] ?>"><?= $nav_item['text'] ?></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="btn btn-outline-info" href="index.php">一覧へ戻る</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+            <main>
